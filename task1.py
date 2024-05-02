8b625ca0852282765b5f7c562de1181fea6200fb
nahidallhv <161585836+nahidallhv@users.noreply.github.com>
2024-05-02 23:57:42 +0400

    .

---
+++ task1.py
@@ -0,0 +1,42 @@
+### 1###
+
+# newList = []
+# for i in range(5):
+#     num = int(input("Ededi daxil edin : "))
+#     newList.append(num)
+# print("Sirasiz list : ", newList)
+# newList.sort()
+# print("Sirali list : ", newList)
+
+### 2 ###
+
+# cumle = input("Bir cümle daxil edin : ")
+# sozler = cumle.split()
+# siraliHerfler = [''.join(sorted(soz)) for soz in sozler]
+# print(" ".join(siraliHerfler))
+
+
+### 3 ###
+
+# n = 13
+# say = 0
+# while True:
+#     num = int(input("Ededi daxil edin : "))
+#     say += 1
+#     if num == n:
+#         print(f"Tebrikler! {say} cehde 13 reqemini tapdız")
+#         break
+
+
+### 4 ###
+
+# print("Sade ededler : ")
+# for i in range(2, 101):
+#     is_prime = True
+#     for j in range(2, i):
+#         if i % j == 0:
+#             is_prime = False
+#             break
+#     if is_prime:
+#         print(i, end=" ")
+
